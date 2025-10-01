7d43600ed35fc3798ee6f6b622ddfd959b697e5d
hugopamponet <hugofarias9@gmail.com>
2025-10-01 17:26:24 -0300

    Funcionalidade aos botões de personalizar

--- js/generatePassword.js
+++ js/generatePassword.js
@@ -1,39 +1,59 @@
-document.querySelector("#toUpdate").addEventListener("click", toUpdate)
-document.querySelector("#copy").addEventListener("click", copy)
-document.querySelector("#copyPassword").addEventListener("click", copy)
+document.querySelector("#toUpdate").addEventListener("click", toUpdate);
+document.querySelector("#copy").addEventListener("click", copy);
+document.querySelector("#copyPassword").addEventListener("click", copy);
 
-const element = document.querySelector("#password")
+const element = document.querySelector("#password");
+const upperCaseCharsElement = document.querySelector("#uppercase-check");
+const numberCharsElement = document.querySelector("#number-check");
+const symbolCharsElement = document.querySelector("#symbol-check");
+
 let passwordLength = 16
 
 function generatePassword() {
-    const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ123456789!@#$%¨&*(){}[]_=-+/*."
+    let chars = "abcdefghjkmnpqrstuvwxyz";
 
-    let password = ""
+    const upperCaseChars = "ABCDEFGHJKMNPQRSTUVWXYZ";
+    const numberChars = "123456789";
+    const symbolChars = "!@#$%¨&*(){}[]_=-+/*.";
+
+    if (upperCaseCharsElement.checked) {
+        chars += upperCaseChars;
+    } else if(numberCharsElement.checked) {
+        chars += numberChars;
+    } else if(symbolCharsElement.checked) {
+        chars += symbolChars;
+    }
+
+    let password = "";
 
     for (let i = 0; i < passwordLength; i++) {
         const randomNumber = Math.floor(Math.random() * chars.length)
         password += chars.substring(randomNumber, randomNumber + 1)
     }
 
-    element.value = password
+    element.value = password;
 }
 
-const passwordLengthElement = document.querySelector("#password-lenght")
+const passwordLengthElement = document.querySelector("#password-lenght");
 passwordLengthElement.addEventListener("input", function () {
-    passwordLength = passwordLengthElement.value
+    passwordLength = passwordLengthElement.value;
 
-    generatePassword()
+    generatePassword();
 })
 
 function copy() {
-    navigator.clipboard.writeText(element.value)
+    navigator.clipboard.writeText(element.value);
 }
 
 function toUpdate() {
-    const toUpdateElement = document.querySelector("#password")
+    const toUpdateElement = document.querySelector("#password");
     toUpdateElement.value = ""
 
-    return generatePassword()
+    return generatePassword();
 }
+
+upperCaseCharsElement.addEventListener("click", generatePassword)
+numberCharsElement.addEventListener("click", generatePassword)
+symbolCharsElement.addEventListener("click", generatePassword)
 
 generatePassword()
